e25e6d4d46d046691101d86af7dcb5ff763ac37d
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-01-25 17:16:11 +0900

    fix exports

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -2,7 +2,7 @@ export { ECDSA } from "./ecdsa.js";
 export { HMAC } from "./hmac.js";
 export { RSASSAPKCS1v1_5, RSASSAPSS } from "./rsa.js";
 export { sha1, sha256, sha384, sha512 } from "./sha.js";
-export { random, generateRandomInteger, generateRandomString } from "./random.js";
+export { random, generateRandomInteger, generateRandomString, alphabet } from "./random.js";
 export { constantTimeEqual } from "./buffer.js";
 
 export type { ECDSACurve } from "./ecdsa.js";
