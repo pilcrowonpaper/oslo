8cd429a2bc1114480583ef726d3c241ea8f9e700
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-04-26 18:02:05 +0900

    fix format

--- src/binary/bytes.ts
+++ src/binary/bytes.ts
@@ -1,7 +1,11 @@
 export function compareBytes(a: Uint8Array, b: Uint8Array): boolean {
-	if (a.byteLength !== b.byteLength) return false;
+	if (a.byteLength !== b.byteLength) {
+		return false;
+	}
 	for (let i = 0; i < b.byteLength; i++) {
-		if (a[i] !== b[i]) return false;
+		if (a[i] !== b[i]) {
+			return false;
+		}
 	}
 	return true;
 }
