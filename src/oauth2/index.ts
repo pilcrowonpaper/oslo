a30dbe5b9e6b9c81ef3cfe19ca38544aa5351719
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-04-27 14:00:08 +0900

    update response body type

--- src/oauth2/index.ts
+++ src/oauth2/index.ts
@@ -297,7 +297,7 @@ interface TokenErrorResponseBody {
 
 export interface TokenResponseBody {
 	access_token: string;
-	token_type?: string;
+	token_type: string;
 	expires_in?: number;
 	refresh_token?: string;
 	scope?: string;
