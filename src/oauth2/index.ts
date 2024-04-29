71bee7e5b39d8a750a17424a10a1123b4e2e76c3
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-04-29 22:58:41 +0900

    rework oauth client

--- src/oauth2/index.ts
+++ src/oauth2/index.ts
@@ -1,6 +1,6 @@
 import { sha256 } from "../crypto/index.js";
 import { base64, base64url } from "../encoding/index.js";
-import { TimeSpan, addToDate } from "../index.js";
+import { addToDate, TimeSpan } from "../index.js";
 
 export class OAuth2Client {
 	public clientId: string;
@@ -8,8 +8,7 @@ export class OAuth2Client {
 	public authorizeEndpoint: string;
 	public tokenEndpoint: string;
 
-	private redirectURI: string | null;
-	private authenticateWith: "http_basic_auth" | "request_body" = "http_basic_auth";
+	private redirectURI: string | null = null;
 
 	constructor(
 		clientId: string,
@@ -17,138 +16,58 @@ export class OAuth2Client {
 		tokenEndpoint: string,
 		options?: {
 			redirectURI?: string;
-			authenticateWith?: "http_basic_auth" | "request_body";
 		}
 	) {
 		this.clientId = clientId;
 		this.authorizeEndpoint = authorizeEndpoint;
 		this.tokenEndpoint = tokenEndpoint;
-		this.redirectURI = options?.redirectURI ?? null;
-		if (options?.authenticateWith !== undefined) {
-			this.authenticateWith = options?.authenticateWith;
+		if (options !== undefined && options.redirectURI !== undefined) {
+			this.redirectURI = options.redirectURI;
 		}
 	}
 
-	public createAuthorizationURL(options?: {
-		state?: string;
-		codeVerifier?: string;
-		codeChallengeMethod?: "S256" | "plain";
-		scopes?: string[];
-	}): URL {
-		const scopes = Array.from(new Set(options?.scopes ?? [])); // remove duplicates
-		const authorizationUrl = new URL(this.authorizeEndpoint);
-		authorizationUrl.searchParams.set("response_type", "code");
-		authorizationUrl.searchParams.set("client_id", this.clientId);
-		if (options?.state !== undefined) {
-			authorizationUrl.searchParams.set("state", options.state);
-		}
-		if (scopes.length > 0) {
-			authorizationUrl.searchParams.set("scope", scopes.join(" "));
-		}
+	public createAuthorizationURL(): OAuth2AuthorizationURL {
+		const url = new OAuth2AuthorizationURL(this.authorizeEndpoint, this.clientId);
 		if (this.redirectURI !== null) {
-			authorizationUrl.searchParams.set("redirect_uri", this.redirectURI);
+			url.setRedirectURI(this.redirectURI);
 		}
-		if (options?.codeVerifier !== undefined) {
-			const codeChallengeMethod = options?.codeChallengeMethod ?? "S256";
-			if (codeChallengeMethod === "S256") {
-				const codeChallengeBuffer = sha256(new TextEncoder().encode(options.codeVerifier));
-				const codeChallenge = base64url.encode(codeChallengeBuffer, {
-					includePadding: false
-				});
-				authorizationUrl.searchParams.set("code_challenge", codeChallenge);
-				authorizationUrl.searchParams.set("code_challenge_method", "S256");
-			} else if (codeChallengeMethod === "plain") {
-				authorizationUrl.searchParams.set("code_challenge", options.codeVerifier);
-				authorizationUrl.searchParams.set("code_challenge_method", "plain");
-			} else {
-				throw new TypeError(`Invalid value for 'codeChallengeMethod': ${codeChallengeMethod}`);
-			}
-		}
-		return authorizationUrl;
+		return url;
 	}
 
-	public async validateAuthorizationCode<_TokenResponseBody extends TokenResponseBody>(
-		authorizationCode: string,
-		options?: {
-			codeVerifier?: string;
-			credentials?: string;
-		}
-	): Promise<_TokenResponseBody> {
-		const body = new URLSearchParams();
-		body.set("code", authorizationCode);
-		body.set("client_id", this.clientId);
-		body.set("grant_type", "authorization_code");
-
+	public createAccessTokenRequest(authorizationCode: string): OAuth2AccessTokenRequest {
+		const request = new OAuth2AccessTokenRequest(
+			this.tokenEndpoint,
+			this.clientId,
+			authorizationCode
+		);
 		if (this.redirectURI !== null) {
-			body.set("redirect_uri", this.redirectURI);
+			request.setRedirectURI(this.redirectURI);
 		}
-		if (options?.codeVerifier !== undefined) {
-			body.set("code_verifier", options.codeVerifier);
-		}
-		return await this.sendTokenRequest<_TokenResponseBody>(body, options);
+		return request;
 	}
 
-	public async refreshAccessToken<_TokenResponseBody extends TokenResponseBody>(
-		refreshToken: string,
-		options?: {
-			credentials?: string;
-			scopes?: string[];
-		}
-	): Promise<_TokenResponseBody> {
-		const body = new URLSearchParams();
-		body.set("refresh_token", refreshToken);
-		body.set("client_id", this.clientId);
-		body.set("grant_type", "refresh_token");
-
-		const scopes = options?.scopes ?? [];
-		if (scopes.length > 0) {
-			body.set("scope", scopes.join(" "));
-		}
-
-		return await this.sendTokenRequest<_TokenResponseBody>(body, options);
+	public createRefreshTokenRequest(refreshToken: string): OAuth2RefreshTokenRequest {
+		const request = new OAuth2RefreshTokenRequest(this.tokenEndpoint, this.clientId, refreshToken);
+		return request;
 	}
 
-	private async sendTokenRequest<_TokenResponseBody extends TokenResponseBody>(
-		body: URLSearchParams,
-		options?: {
-			credentials?: string;
-		}
+	public async sendTokenRequest<_TokenResponseBody extends TokenResponseBody>(
+		request: OAuth2Request
 	): Promise<_TokenResponseBody> {
-		const headers = new Headers();
-		headers.set("Content-Type", "application/x-www-form-urlencoded");
-		headers.set("Accept", "application/json");
-		headers.set("User-Agent", "oslo");
-
-		if (options?.credentials !== undefined) {
-			const encodedCredentials = base64.encode(
-				new TextEncoder().encode(`${this.clientId}:${options.credentials}`)
-			);
-			headers.set("Authorization", `Basic ${encodedCredentials}`);
-			if (this.authenticateWith === "http_basic_auth") {
-				const encodedCredentials = base64.encode(
-					new TextEncoder().encode(`${this.clientId}:${options.credentials}`)
-				);
-				headers.set("Authorization", `Basic ${encodedCredentials}`);
-			} else if (this.authenticateWith === "request_body") {
-				body.set("client_secret", options.credentials);
-			}
-		}
-
-		const request = new Request(this.tokenEndpoint, {
-			method: "POST",
-			headers,
-			body
-		});
-		const response = await fetch(request);
+		const response = await fetch(request.createFetchRequest());
 		const result: _TokenResponseBody | TokenErrorResponseBody = await response.json();
-
-		// providers are allowed to return non-400 status code for errors
-		if (!("access_token" in result) && "error" in result) {
-			throw new OAuth2RequestError(request, result);
-		} else if (!response.ok) {
-			throw new OAuth2RequestError(request, {});
+		if ("access_token" in result) {
+			return result as _TokenResponseBody;
 		}
-		return result;
+		if ("error_description" in result) {
+			throw new OAuth2RequestError(request, {
+				message: result.error,
+				description: result.error_description
+			});
+		}
+		throw new OAuth2RequestError(request, {
+			message: result.error
+		});
 	}
 }
 
@@ -156,79 +75,36 @@ export class OAuth2TokenRevocationClient {
 	public clientId: string;
 	public tokenRevocationEndpoint: string;
 
-	private authenticateWith: "http_basic_auth" | "request_body" = "http_basic_auth";
-
-	constructor(
-		clientId: string,
-		tokenRevocationEndpoint: string,
-		options?: {
-			authenticateWith?: "http_basic_auth" | "request_body";
-		}
-	) {
+	constructor(clientId: string, tokenRevocationEndpoint: string) {
 		this.clientId = clientId;
 		this.tokenRevocationEndpoint = tokenRevocationEndpoint;
-		if (options?.authenticateWith !== undefined) {
-			this.authenticateWith = options.authenticateWith;
-		}
 	}
 
-	public async revokeAccessToken(
-		accessToken: string,
-		options?: {
-			credentials?: string;
-		}
-	): Promise<void> {
-		const body = new URLSearchParams();
-		body.set("token", accessToken);
-		body.set("token_type_hint", "access_token");
-		await this.sendRevocationRequest(body, options);
+	public createAccessTokenRevocationRequest(accessToken: string): OAuth2TokenRevocationRequest {
+		const request = new OAuth2TokenRevocationRequest(
+			this.clientId,
+			this.tokenRevocationEndpoint,
+			accessToken
+		);
+		request.setTokenTypeHint("access_token");
+		return request;
 	}
 
-	public async revokeRefreshToken(
-		refreshToken: string,
-		options?: {
-			credentials?: string;
-		}
-	): Promise<void> {
-		const body = new URLSearchParams();
-		body.set("token", refreshToken);
-		body.set("token_type_hint", "refresh_token");
-		await this.sendRevocationRequest(body, options);
+	public createRefreshTokenRevocationRequest(refreshToken: string): OAuth2TokenRevocationRequest {
+		const request = new OAuth2TokenRevocationRequest(
+			this.clientId,
+			this.tokenRevocationEndpoint,
+			refreshToken
+		);
+		request.setTokenTypeHint("refresh_token");
+		return request;
 	}
 
-	private async sendRevocationRequest(
-		body: URLSearchParams,
-		options?: {
-			credentials?: string;
+	public async sendTokenRevocationRequest(request: OAuth2TokenRevocationRequest): Promise<void> {
+		const response = await fetch(request.createFetchRequest());
+		if (response.status === 200) {
+			return;
 		}
-	): Promise<void> {
-		const headers = new Headers();
-		headers.set("Content-Type", "application/x-www-form-urlencoded");
-		headers.set("Accept", "application/json");
-		headers.set("User-Agent", "oslo");
-
-		if (options?.credentials !== undefined) {
-			const encodedCredentials = base64.encode(
-				new TextEncoder().encode(`${this.clientId}:${options.credentials}`)
-			);
-			headers.set("Authorization", `Basic ${encodedCredentials}`);
-			if (this.authenticateWith === "http_basic_auth") {
-				const encodedCredentials = base64.encode(
-					new TextEncoder().encode(`${this.clientId}:${options.credentials}`)
-				);
-				headers.set("Authorization", `Basic ${encodedCredentials}`);
-			} else if (this.authenticateWith === "request_body") {
-				body.set("client_secret", options.credentials);
-			}
-		}
-
-		const request = new Request(this.tokenRevocationEndpoint, {
-			method: "POST",
-			headers,
-			body
-		});
-		const response = await fetch(request);
-
 		if (response.status === 503) {
 			const retryAfterHeader = response.headers.get("Retry-After");
 			if (retryAfterHeader === null) {
@@ -248,14 +124,185 @@ export class OAuth2TokenRevocationClient {
 			}
 			throw new OAuth2TokenRevocationRetryError();
 		}
-
 		const result: TokenErrorResponseBody = await response.json();
-		if (!("access_token" in result) && "error" in result) {
-			throw new OAuth2RequestError(request, result);
-		} else if (!response.ok) {
-			throw new OAuth2RequestError(request, {});
+		if ("error_description" in result) {
+			throw new OAuth2RequestError(request, {
+				message: result.error,
+				description: result.error_description
+			});
 		}
+		throw new OAuth2RequestError(request, {
+			message: result.error
+		});
 	}
+}
+
+export class OAuth2AuthorizationURL extends URL {
+	constructor(url: string, clientId: string) {
+		super(url);
+		this.searchParams.set("response_type", "code");
+		this.searchParams.set("client_id", clientId);
+	}
+
+	public setRedirectURI(redirectURI: string): this {
+		this.searchParams.set("redirect_uri", redirectURI);
+		return this;
+	}
+
+	public setScopes(...scopes: string[]): this {
+		let scopeValue = scopes.join(" ");
+		const existingScopes = this.searchParams.get("scope");
+		if (existingScopes !== null) {
+			scopeValue = " " + existingScopes;
+		}
+		this.searchParams.set("scope", scopeValue);
+		return this;
+	}
+
+	public setState(state: string): this {
+		this.searchParams.set("state", state);
+		return this;
+	}
+
+	public setS256CodeChallenge(codeVerifier: string): this {
+		const codeChallengeBytes = sha256(new TextEncoder().encode(codeVerifier));
+		const codeChallenge = base64url.encode(codeChallengeBytes, {
+			includePadding: false
+		});
+		this.searchParams.set("code_challenge", codeChallenge);
+		this.searchParams.set("code_challenge_method", "S256");
+		return this;
+	}
+
+	public setPlainCodeChallenge(codeVerifier: string): this {
+		this.searchParams.set("code_challenge", codeVerifier);
+		this.searchParams.set("code_challenge_method", "plain");
+		return this;
+	}
+}
+
+export class OAuth2Request {
+	public method = "POST";
+	public body = new URLSearchParams();
+	public headers = new Headers();
+
+	public url: string;
+	public clientId: string;
+
+	constructor(url: string, clientId: string) {
+		this.url = url;
+		this.clientId = clientId;
+		this.body.set("client_id", clientId);
+		this.headers.set("Content-Type", "application/x-www-form-urlencoded");
+		this.headers.set("Accept", "application/json");
+		this.headers.set("User-Agent", "oslo");
+	}
+
+	public authenticateWithRequestBody(clientSecret: string): this {
+		this.body.set("client_secret", clientSecret);
+		return this;
+	}
+
+	public authenticateWithHTTPBasicAuth(clientPassword: string): this {
+		const authorizationHeader = base64.encode(
+			new TextEncoder().encode(`${this.clientId}:${clientPassword}`)
+		);
+		this.headers.set("Authorization", authorizationHeader);
+		return this;
+	}
+
+	public createFetchRequest(): Request {
+		return new Request(this.url, {
+			method: "POST",
+			body: this.body,
+			headers: this.headers
+		});
+	}
+}
+
+export class OAuth2AccessTokenRequest extends OAuth2Request {
+	constructor(url: string, clientId: string, code: string) {
+		super(url, clientId);
+		this.body.set("grant_type", "authorization_code");
+		this.body.set("code", code);
+	}
+
+	public setCodeVerifier(codeVerifier: string): this {
+		this.body.set("code_verifier", codeVerifier);
+		return this;
+	}
+
+	public setRedirectURI(redirectURI: string): this {
+		this.body.set("redirect_uri", redirectURI);
+		return this;
+	}
+}
+
+export class OAuth2RefreshTokenRequest extends OAuth2Request {
+	constructor(url: string, clientId: string, code: string) {
+		super(url, clientId);
+		this.body.set("grant_type", "refresh_token");
+		this.body.set("code", code);
+	}
+
+	public setScopes(...scopes: string[]): this {
+		let scopeValue = scopes.join(" ");
+		const existingScopes = this.body.get("scope");
+		if (existingScopes !== null) {
+			scopeValue = " " + existingScopes;
+		}
+		this.body.set("scope", scopeValue);
+		return this;
+	}
+}
+
+export class OAuth2TokenRevocationRequest extends OAuth2Request {
+	constructor(url: string, clientId: string, token: string) {
+		super(url, clientId);
+		this.body.set("token", token);
+	}
+
+	public setTokenTypeHint(tokenTypeHint: "access_token" | "refresh_token"): this {
+		this.body.set("token_type_hint", tokenTypeHint);
+		return this;
+	}
+}
+
+export class OAuth2RequestError extends Error {
+	public request: OAuth2Request;
+	public description: string | null;
+	constructor(
+		request: OAuth2Request,
+		options?: {
+			message?: string;
+			description?: string;
+		}
+	) {
+		super(options?.message ?? "Unknown error");
+		this.request = request;
+		this.description = options?.description ?? null;
+	}
+}
+
+export class OAuth2TokenRevocationRetryError extends Error {
+	public retryAfter: Date | null;
+	constructor(options?: { retryAfter?: Date }) {
+		super("retry revocation");
+		this.retryAfter = options?.retryAfter ?? null;
+	}
+}
+
+export interface TokenResponseBody {
+	access_token: string;
+	token_type: string;
+	expires_in?: number;
+	refresh_token?: string;
+	scope?: string;
+}
+
+interface TokenErrorResponseBody {
+	error: string;
+	error_description?: string;
 }
 
 export function generateCodeVerifier(): string {
@@ -272,37 +319,6 @@ export function generateState(): string {
 	return base64url.encode(randomValues, {
 		includePadding: false
 	});
-}
-
-export class OAuth2RequestError extends Error {
-	public request: Request;
-	public description: string | null;
-	constructor(request: Request, body: Partial<TokenErrorResponseBody>) {
-		super(body.error ?? "");
-		this.request = request;
-		this.description = body.error_description ?? null;
-	}
-}
-
-export class OAuth2TokenRevocationRetryError extends Error {
-	public retryAfter: Date | null;
-	constructor(options?: { retryAfter?: Date }) {
-		super("retry revocation");
-		this.retryAfter = options?.retryAfter ?? null;
-	}
-}
-
-interface TokenErrorResponseBody {
-	error: string;
-	error_description?: string;
-}
-
-export interface TokenResponseBody {
-	access_token: string;
-	token_type: string;
-	expires_in?: number;
-	refresh_token?: string;
-	scope?: string;
 }
 
 function parseDateString(dateString: string): Date | null {
